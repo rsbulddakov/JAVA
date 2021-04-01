52e63508b1d6f0fd3827d87caab4d344e7393564
Ruslan <hun@github.com>
2021-04-02 00:59:51 +0300

    lesson_2_6 complete

--- src/GU/Java/communication/client/Client.java
+++ src/GU/Java/communication/client/Client.java
@@ -1,4 +1,54 @@
 package GU.Java.communication.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Client {
+    private static SimpleDateFormat formatForDateNow = new SimpleDateFormat("HH:mm:ss '| server: '");
+    public Client() {
+
+        try {
+            Socket socket = new Socket("127.0.0.1", 8080);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            Scanner scanner = new Scanner(System.in);
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        out.writeUTF(scanner.nextLine());
+                    } catch (IOException e) {
+                        System.out.println("Server down...");
+                        System.out.println("Connection closed.");
+                        break;
+                    }
+                }
+            })
+                    .start();
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String message = in.readUTF();
+                        Date dateNow = new Date();
+                        System.out.println(formatForDateNow.format(dateNow) + message);
+                    } catch (IOException e) {
+                        System.out.println("Incoming message channel closed.");
+                        System.out.println("Please double press ENTER...");
+                        break;
+                    }
+                }
+            })
+                    .start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/GU/Java/communication/client/Main.java
+++ src/GU/Java/communication/client/Main.java
@@ -1,4 +1,7 @@
 package GU.Java.communication.client;
 
 public class Main {
+    public static void main(String[] args) {
+        new Client();
+    }
 }
